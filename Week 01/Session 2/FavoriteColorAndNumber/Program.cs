e4e9f45a4226fa2b7a93a816e465daf704938a73
dxsigm <dxsigm@rit.edu>
2023-08-31 09:19:20 -0400

    Session 1-2

--- Week 01/Session 2/FavoriteColorAndNumber/Program.cs
+++ Week 01/Session 2/FavoriteColorAndNumber/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace ColorsAndNumbers
 {
     namespace Colors
@@ -40,11 +39,84 @@ namespace ColorsAndNumbers
 
 namespace FavoriteColorAndNumber
 {
-    internal class Program
+    using ColorsAndNumbers.Colors;
+
+    using ColorsAlias = ColorsAndNumbers.Colors;
+
+
+    static internal class Program
     {
+
+        static void WriteMyVar(ref int nParameter)
+        {
+            Console.WriteLine(nParameter);
+
+            nParameter = 42;
+        }
+
         static void Main(string[] args)
         {
-            EColors eColors;
+            ColorsAlias.EColors eColors;
+
+
+
+            Console.WriteLine("");
+
+            int myInt = 0;
+            int anotherInt = 2;
+            string sFavoriteColor = null;
+            string sFavoriteNumber = "";
+            bool bValid = false;
+            int? nFavoriteNumber = null;
+
+            anotherInt = myInt;
+
+            WriteMyVar(ref myInt);
+
+
+            // 3 errors
+            // 1. syntax errors (missing semi)
+            // 2. logical (code works but doesn't do the right thing)
+            // 3. run-time errors (code crashes!)
+
+            // prompt the user for their favorite color
+            //Console.Write("Hello!  Please enter your favorite color: ")
+            // this was a syntax error: missing semi
+            Console.Write("Hello!  Please enter your favorite color: ");
+
+            // read the favorite color form the user and store it in a variable
+            sFavoriteColor = Console.ReadLine();
+
+            // prompt the user for their favorite number
+            Console.Write("Please enter your favorite number: ");
+
+            // read their favrotie num into a variable
+            sFavoriteNumber = Console.ReadLine();
+
+            //while (bValid == false)
+            //{
+            //}
+
+            while (nFavoriteNumber == null)
+            {
+                try
+                {
+                    nFavoriteNumber = Convert.ToInt32(sFavoriteNumber);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+
+            }
+
+
+
+            // a loop that outputs their fav color fav num times
+
+
+
+
         }
     }
 }
