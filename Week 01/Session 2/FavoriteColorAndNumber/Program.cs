a1d89581c309fc683b4fa17a3ddc43e53903591a
dxsigm <dxsigm@rit.edu>
2023-08-31 07:41:08 -0400

    Session 1-2

---
+++ Week 01/Session 2/FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ColorsAndNumbers
+{
+    namespace Colors
+    {
+        enum EColors
+        {
+            red,
+            orange,
+            yellow,
+            green,
+            blue,
+            indigo,
+            violet
+        }
+    }
+
+    namespace Numbers
+    {
+        enum ENumbers
+        {
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven
+        }
+
+    }
+}
+
+
+namespace FavoriteColorAndNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EColors eColors;
+        }
+    }
+}
