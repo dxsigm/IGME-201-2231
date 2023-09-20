aeea570421d0096c2f60995a37c94da434d553ac
dxsigm <dxsigm@rit.edu>
2023-09-19 23:31:27 -0400

    Session 4-1

---
+++ Week 04/Session 1/MathDelegate/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDelegate
+{
+    /// delegate steps
+    /// 1. define the delegate method data type based on the method signature
+    ///         delegate double MathFunction(double n1, double n2);
+    /// 2. declare the delegate method variable
+    ///         MathFunction processDivMult;
+    /// 3. point the variable to the method that it should call
+    ///         processDivMult = new MathFunction(Multiply);
+    /// 4. call the delegate method
+    ///         nAnswer = processDivMult(n1, n2);
+
+    // define a custom delegate data type whose variable can point to a method that
+    // accepts 2 doubles and returns a double
+    delegate double MathFunction(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            string sOperation = null;
+
+            double nNum1 = 0;
+            double nNum2 = 0;
+            double nAnswer = 0;
+
+            int[] aInt = new int[5];
+            int[] bInt;
+
+            bInt = aInt;
+
+
+            // prompt and convert first number
+            do
+            {
+                Console.Write("Enter a number: ");
+                sNumber = Console.ReadLine();
+            } while (!double.TryParse(sNumber, out nNum1));
+
+            // prompt and convert second number
+            do
+            {
+                Console.Write("Enter another number: ");
+                sNumber = Console.ReadLine();
+            } while (!double.TryParse(sNumber, out nNum2));
+
+            // prompt for operation
+            do
+            {
+                Console.Write("Multiply or Divide: ");
+                sOperation = Console.ReadLine();
+            } while (!sOperation.ToLower().StartsWith("m") &&
+                     !sOperation.ToLower().StartsWith("d"));
+
+            // declare a variable of our custom delegate type
+            //MathFunction processDivMult;
+
+            // use the built-in method delegate type that returns a value
+            // the last type in the <> is the return data type
+            Func<double, double, double> processDivMult;
+
+            // if the method does not return data, then use the Action<> generic delegate type
+            Action<double> outputAnswer;
+            outputAnswer = new Action<double>(OutputAnswer);
+
+            if (sOperation.ToLower().StartsWith("m"))
+            {
+                // we could of course simply call Multiply()
+                //nAnswer = Multiply(nNum1, nNum2);
+
+                // but let's use a delegate variable to call the intended method
+                // because it's fun!  (and because it's used everywhere in UI development)
+
+                // the most explicit approach using the 4 steps at the top
+                //processDivMult  = new MathFunction(Multiply);
+
+                // use the built-in C# generic template delegate type
+                processDivMult = new Func<double, double, double>(Multiply);
+
+                // or we can just set the variable to the method without using the constructor
+                processDivMult = Multiply;
+
+                // use an anonymous codeblock using the delegate keyword
+                processDivMult = delegate (double n1, double n2)
+                {
+                    double returnVal = 0;
+                    returnVal = n1 * n2;
+                    return returnVal;
+                };
+
+                // use an anonymous codeblock using the lambda operator "=>"
+                processDivMult = (double n1, double n2) =>
+                {
+                    double returnVal = 0;
+                    returnVal = n1 * n2;
+                    return returnVal;
+                };
+
+                // abbreviate the lambda expression
+                processDivMult = (n1, n2) =>
+                {
+                    double returnVal = 0;
+                    returnVal = n1 * n2;
+                    return returnVal;
+                };
+
+                // further abbreviate the lambda expression
+                processDivMult = (n1, n2) =>
+                {
+                    return n1 * n2;
+                };
+
+                // most abstruse abbreviation of the lambda expression
+                // please don't write this code!
+                // the goal is always to write code that documents itself as much as possible
+                processDivMult = (n1, n2) => n1 * n2;
+            }
+            else
+            {
+                //nAnswer = Divide(nNum1, nNum2);
+                //processDivMult = new MathFunction(Divide);
+                processDivMult = new Func<double, double, double>(Divide);
+            }
+
+            // call the method via the delegate variable
+            nAnswer = processDivMult(nNum1, nNum2);
+
+            // use the Action<> delegate to output the answer
+            outputAnswer(nAnswer);
+
+        }
+
+        static void OutputAnswer(double dAnswer)
+        {
+            Console.WriteLine("The answer is: {0}", dAnswer);
+        }
+
+
+        static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        static double Sum(double n1, double n2, double n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+    }
+}
---
+++ Week 04/Session 1/MemoryGame/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+/// delegate steps
+/// 1. define the delegate method data type based on the method signature
+///         delegate double MathFunction(double n1, double n2);
+/// 2. declare the delegate method variable
+///         MathFunction processDivMult;
+/// 3. point the variable to the method that it should call
+///         processDivMult = new MathFunction(Multiply);
+/// 4. call the delegate method / add the delegate method to the object's event handler
+///         nAnswer = processDivMult(n1, n2);
+///         timer.Elapsed += TimesUp;
+
+namespace MemoryGame
+{
+    static internal class Program
+    {
+        // class-level variables that are available to every method in the class
+        static Timer timeOutTimer;
+        static bool bTimeOut = false;
+
+        static void Main(string[] args)
+        {
+
+        start:
+
+            string displayString = "";
+            Random rand = new Random();
+            Console.Clear();
+
+            while (!bTimeOut)
+            {
+                // add a random letter to the current display string
+                displayString += (char)('A' + rand.Next(0, 26));
+
+                // iterate through the string to output each character
+                foreach (char c in displayString)
+                {
+                    Console.Write(c);
+
+                    // 0.5 second delay between outputting each character
+                    System.Threading.Thread.Sleep(500);
+                }
+
+                Console.Clear();
+
+                timeOutTimer = new Timer(displayString.Length * 500 + 1000);
+
+                // Timer calls the Timer.Elapsed event handler when the time elapses
+                // The Timer.Elapsed event handler uses a delegate function with the following signature:
+                //        public delegate void ElapsedEventHandler(object sender, ElapsedEventArgs e);
+                // This delegate method type is already defined for us by .NET
+                timeOutTimer.Elapsed += new ElapsedEventHandler(TimesUp);
+
+                // start the timer just before the user is prompted for the answer
+                // the timer process will be a second process running at the same time as this program
+                // it will countdown from the value initialized in line #48 and call the delegate method(s)
+                // associated with the Elapsed event handler as set in line #59
+                timeOutTimer.Start();
+
+                string sAnswer = null;
+                sAnswer = Console.ReadLine();
+
+                // always stop the timer directly after the Console.ReadLine()
+                // otherwise the timer will keep running even if the user entered the correct answer in time
+                // and the TimesUp() method will be called
+                timeOutTimer.Stop();
+
+                // if the correct character sequence was entered and the timer did not expire
+                if (sAnswer.ToUpper() == displayString && !bTimeOut /* same as bTimeOut == false */ )
+                {
+                    Console.WriteLine("Well Done!  Your current score is {0}", displayString.Length);
+                }
+                else
+                {
+                    // otherwise they entered the wrong answer or the timer expired
+                    Console.WriteLine("Bad luck.  :(  The correct code was {0}.  Your final score is: {1}", displayString, displayString.Length - 1);
+
+                    // set timeout to leave the while() loop
+                    bTimeOut = true;
+                }
+            }
+
+            Console.Write("Press Enter to Play Again");
+            Console.ReadLine();
+
+            bTimeOut = false;
+            goto start;
+
+        }
+
+        static void TimesUp(object sender, ElapsedEventArgs e)
+        {
+            // send a newline to the console to interrupt the user entry
+            Console.WriteLine();
+
+            // let the user know their time is up
+            Console.WriteLine("Your time is up!");
+
+            // ask them to press enter to get out of the Console.ReadLine() at line #68
+            Console.WriteLine("Please press Enter.");
+
+            // set the time out flag
+            bTimeOut = true;
+
+            // stop the timer, otherwise it will start over
+            timeOutTimer.Stop();
+        }
+    }
+}
