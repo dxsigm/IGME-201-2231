5a333e3ef1d33edb13cde83dce5f7a0c831d67ca
dxsigm <dxsigm@rit.edu>
2023-09-28 09:19:46 -0400

    Session 5-2

---
+++ Week 05/Session 2/Factorial/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            int nNumber = 0;
+            int nAnswer = 0;
+
+            do
+            {
+                Console.Write("Enter a positive integer: ");
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nNumber) && nNumber <= 0);
+
+            nAnswer = 1;
+
+            while( nNumber > 0)
+            {
+                nAnswer *= nNumber;
+                --nNumber;
+            }
+
+            nAnswer = Factorial(nNumber);
+
+
+            // example of variable number of parameters passed to a method
+            double a1 = Average(2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            double a2 = Average(1, 5, 4, 3, 8, 1);
+
+        }
+
+        // the params keyword converts the list of parameters to an array
+        // they must all be the same data type
+        static double Average(int nRoundTo, params int[] aInt)
+        {
+            double avg = 0;
+            int sum = 0;
+
+            foreach(int i in aInt)
+            {
+                sum += i;
+            }
+
+            avg = Math.Round((double)sum / aInt.Length, nRoundTo);
+
+            return avg;
+        }
+        
+
+        static int Factorial(int nNumber)
+        {
+            int nAnswer = 1;
+
+            if (nNumber == 0)
+            {
+                nAnswer = 1;
+            }
+            else
+            {
+                nAnswer = nNumber * Factorial(nNumber - 1);
+            }
+
+            return (nAnswer);
+        }
+
+        //Factorial(2) = 2 * f(1) : waiting at line 44
+        //Factorial(1) = 1 * f(0) : waiting at line 44
+        //Factorial(0) = 1
+        //
+        //Factorial(1) = 1 * 1 = 1
+        //Factorial(2) = 2 * 1 = 2
+
+    }
+}
---
+++ Week 05/Session 2/SortedList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> dPersonAge = new Dictionary<string,int>();
+            // Dictionary<string,StudentStruct> dStudent = new Dictionary<string,StudentStruct>();
+
+            dPersonAge.Add("sue", 84);
+            // dStudent.Add("Max", maxStudent);
+            // dStudent["Max"] = maxStudent;
+
+            dPersonAge["david"] = 50;
+            //this does the same as the line above
+            dPersonAge.Add("david", 50);
+
+
+            // cannot add the same key twice
+            //dPersonAge.Add("david",60);
+            dPersonAge["david"] = 60;
+
+            foreach( KeyValuePair<string,int> valuePair in dPersonAge)
+            {
+                Console.WriteLine($"person[{valuePair.Key}] = {valuePair.Value}");
+            }
+
+
+            SortedList<string,int> personAge = new SortedList<string,int>();
+            personAge.Add("sue", 84);
+            personAge["david"] = 50;
+            personAge["joe"] = 80;
+
+            personAge.Remove("joe");
+
+            foreach (KeyValuePair<string, int> valuePair in personAge)
+            {
+                Console.WriteLine($"person[{valuePair.Key}] = {valuePair.Value}");
+            }
+        }
+    }
+}
