394521e6fff9ddac91c5efc5fb2f31f5adf982bd
dxsigm <dxsigm@rit.edu>
2023-10-12 19:14:12 -0400

    Session 7-1

--- Week 06/Session 2/Shape/Program.cs
+++ Week 06/Session 2/Shape/Program.cs
@@ -17,7 +17,6 @@ namespace Shape
     {
         public virtual void DrawMe()
         {
-
         }
     }
 
---
+++ Week 07/Session 1/CourseLib/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib
+{
+    public class Courses
+    {
+        SortedList<string, Course> sortedList = new SortedList<string, Course>();
+
+        public void Remove(string courseCode)
+        {
+            if (courseCode != null)
+            {
+                sortedList.Remove(courseCode);
+            }
+        }
+
+        public Course this[string courseCode]
+        {
+            get
+            {
+                Course returnVal;
+                try
+                {
+                    returnVal = (Course)sortedList[courseCode];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
+
+                return (returnVal);
+            }
+
+            set
+            {
+                try
+                {
+                    sortedList[courseCode] = value;
+                }
+                catch
+                {
+                    // an exception will be raised if an entry with a duplicate key is added
+                    // duplicate key handling (currently ignoring any exceptions)
+                }
+            }
+        }
+    }
+
+    public class Course 
+    { 
+    }
+}
