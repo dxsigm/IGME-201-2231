2410183a5e650ef006b0d18f5b8ae6df06cdd611
dxsigm <dxsigm@rit.edu>
2023-10-24 09:53:22 -0400

    Session 9-1

---
+++ Week 09/Session 1/EditPerson/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditPerson
+{
+    public partial class PersonEditForm : Form
+    {
+        public PersonEditForm()
+        {
+            /******************************************************************************************
+             **************THIS MUST BE THE FIRST FUNCTION CALL IN YOUR FORM CONSTRUCTOR **************
+             ******************************************************************************************/
+            InitializeComponent();
+
+            foreach (Control control in this.Controls)
+            {
+                // use Tag property to indicate valid state
+                control.Tag = false;
+            }
+
+            this.okButton.Enabled = false;
+
+            this.nameTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.emailTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.ageTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.specTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.gpaTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+            this.licTextBox.Validating += new CancelEventHandler(TxtBoxEmpty__Validating);
+
+            this.nameTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+            this.emailTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+            this.ageTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+            this.specTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+            this.gpaTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+            this.licTextBox.TextChanged += new EventHandler(TxtBoxEmpty__TextChanged);
+
+            this.ageTextBox.KeyPress += new KeyPressEventHandler(TxtBoxNum__KeyPress);
+            this.licTextBox.KeyPress += new KeyPressEventHandler(TxtBoxNum__KeyPress);
+            this.gpaTextBox.KeyPress += new KeyPressEventHandler(TxtBoxNum__KeyPress);
+
+            this.typeComboBox.SelectedIndexChanged += new EventHandler(TypeComboBox__SelectedIndexChanged);
+
+            this.cancelButton.Click += new EventHandler(CancelButton__Click);
+
+
+        }
+
+        private void CancelButton__Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+
+        private void TypeComboBox__SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+            if( cb.SelectedIndex == 0 )
+            {
+                this.specialtyLabel.Visible = false;
+                this.specTextBox.Visible = false;
+
+                this.specTextBox.Tag = true;
+
+                this.gpaLabel.Visible = true;
+                this.gpaTextBox.Visible = true;
+
+                this.gpaTextBox.Tag = (this.gpaTextBox.Text.Length > 0);
+            }
+            else
+            {
+                this.specialtyLabel.Visible = true;
+                this.specTextBox.Visible = true;
+
+                this.gpaTextBox.Tag = true;
+
+                this.gpaLabel.Visible = false;
+                this.gpaTextBox.Visible = false;
+
+                this.specTextBox.Tag = (this.specTextBox.Text.Length > 0);
+            }
+
+            ValidateAll();
+        }
+
+        private void TxtBoxEmpty__TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if( tb.Text.Length == 0)
+            {
+                this.errorProvider1.SetError(tb, "This field cannot be empty");
+                tb.Tag = false;
+            }
+            else
+            {
+                this.errorProvider1.SetError(tb, null);
+                tb.Tag = true;
+            }
+
+            ValidateAll();
+        }
+
+        private void TxtBoxNum__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if( Char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+                tb.Tag = true;
+            }
+            else
+            {
+                e.Handled = true;
+
+                if( tb == this.gpaTextBox)
+                {
+                    if( e.KeyChar == '.' && !tb.Text.Contains("."))
+                    {
+                        e.Handled = false;
+                        tb.Tag = true;
+                    }
+                }
+            }
+
+            ValidateAll();
+        }
+
+
+        public void TxtBoxEmpty__Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if(tb.Text.Length == 0)
+            {
+                this.errorProvider1.SetError(tb, "This field cannot be empty.");
+                e.Cancel = true;
+                tb.Tag = false;
+            }
+            else
+            {
+                this.errorProvider1.SetError(tb, null);
+                e.Cancel = false;
+                tb.Tag = true;
+            }
+
+            ValidateAll();
+        }
+
+        private void ValidateAll()
+        {
+            this.okButton.Enabled =
+                (bool)this.nameTextBox.Tag &&
+                (bool)this.emailTextBox.Tag &&
+                (bool)this.ageTextBox.Tag &&
+                (bool)this.specTextBox.Tag &&
+                (bool)this.gpaTextBox.Tag;
+        }
+
+
+        public void typeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Week 09/Session 1/PeopleList/PeopleList.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PeopleList
+{
+    partial class PeopleList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.peopleListView = new System.Windows.Forms.ListView();
+            this.nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.emailHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ageHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.licHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gpa_specHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.exitButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // peopleListView
+            // 
+            this.peopleListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameHeader,
+            this.emailHeader,
+            this.ageHeader,
+            this.licHeader,
+            this.gpa_specHeader});
+            this.peopleListView.FullRowSelect = true;
+            this.peopleListView.GridLines = true;
+            this.peopleListView.HideSelection = false;
+            this.peopleListView.Location = new System.Drawing.Point(11, 11);
+            this.peopleListView.Margin = new System.Windows.Forms.Padding(2);
+            this.peopleListView.MultiSelect = false;
+            this.peopleListView.Name = "peopleListView";
+            this.peopleListView.Size = new System.Drawing.Size(788, 301);
+            this.peopleListView.TabIndex = 1;
+            this.peopleListView.UseCompatibleStateImageBehavior = false;
+            this.peopleListView.View = System.Windows.Forms.View.Details;
+            // 
+            // nameHeader
+            // 
+            this.nameHeader.Text = "Name";
+            this.nameHeader.Width = 192;
+            // 
+            // emailHeader
+            // 
+            this.emailHeader.Text = "Email";
+            this.emailHeader.Width = 185;
+            // 
+            // ageHeader
+            // 
+            this.ageHeader.Text = "Age";
+            // 
+            // licHeader
+            // 
+            this.licHeader.Text = "License Id";
+            this.licHeader.Width = 107;
+            // 
+            // gpa_specHeader
+            // 
+            this.gpa_specHeader.Text = "GPA/Specialty";
+            this.gpa_specHeader.Width = 440;
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(520, 338);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(56, 24);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(371, 338);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(56, 24);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(199, 338);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(56, 24);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // PeopleList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 383);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.peopleListView);
+            this.Name = "PeopleList";
+            this.Text = "PeopleList";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView peopleListView;
+        private System.Windows.Forms.ColumnHeader nameHeader;
+        private System.Windows.Forms.ColumnHeader emailHeader;
+        private System.Windows.Forms.ColumnHeader ageHeader;
+        private System.Windows.Forms.ColumnHeader licHeader;
+        private System.Windows.Forms.ColumnHeader gpa_specHeader;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button addButton;
+    }
+}
